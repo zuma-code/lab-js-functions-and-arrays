c64a962b83b5b7f997b84bbb0f88df0dfa086307
Uros Cirkovic <cirkovic.uros.srb@gmail.com>
2024-01-11 18:24:24 +0100

    initial commit

---
+++ src/functions-and-arrays.js
@@ -0,0 +1,34 @@
+// Iteration 1 | Find the Maximum
+function maxOfTwoNumbers() {}
+
+
+
+
+// Iteration 2 | Find the Longest Word
+const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
+
+function findLongestWord() {}
+
+
+
+
+// Iteration 3 | Sum Numbers
+const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+
+function sumNumbers() {}
+
+
+
+
+// Iteration 4 | Numbers Average
+const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
+
+function averageNumbers() {}
+
+
+
+
+// Iteration 5 | Find Elements
+const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
+
+function doesWordExist() {}
